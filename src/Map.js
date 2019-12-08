2d36ed0bcf98b8092e984cd1959ce766799fdfae
zoecq001 <zoecq001@gmail.com>
2019-12-08 17:50:21 -0500

    show map

---
+++ src/Map.js
@@ -0,0 +1,35 @@
+
+import React from 'react'
+import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
+import './Map.css';
+
+class Map extends React.Component {
+  render() {
+    return (
+      <LeafletMap
+      // This is the default lon and lat of GT
+        center={[33.775620, -84.396286]}
+        zoom={17}
+        maxZoom={19}
+        minZoom={14}
+        attributionControl={true}
+        zoomControl={true}
+        doubleClickZoom={true}
+        scrollWheelZoom={true}
+        dragging={true}
+        animate={true}
+        easeLinearity={0.35}
+      >
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={[33.775620, -84.396286]}>
+          <Popup>
+            Popup for any custom information.
+          </Popup>
+        </Marker>
+      </LeafletMap>
+    );
+  }
+}
+export default Map;
