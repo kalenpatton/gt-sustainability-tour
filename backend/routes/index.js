344850c8e673d27ebe4eedc1cf886920505468ec
Bradley Dover <dover.bradley@gmail.com>
2020-02-21 14:58:53 -0500

    Updated index page

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,11 +4,10 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("Responding to root route")
-  /* res.render('index', {
+  res.render('index', {
     title: 'GT Sustainability Self-guided Tour Backend',
     routes: '/locations, /locations/:id'
-  }); */
-  res.send("string")
+  });
 });
 
 module.exports = router;
